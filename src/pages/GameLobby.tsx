1c488afe9d0422761f87546e98f630a01db52a2a
MineKing <mineking9534@gmail.com>
2024-04-21 23:01:40 +0200

    Add (Verbindung getrennt)

--- src/pages/GameLobby.tsx
+++ src/pages/GameLobby.tsx
@@ -32,7 +32,7 @@ function PlayerList() {
 								<Tooltip
 									placement="right-end"
 									className="font-bold"
-									content={ p.id === player.id ? "Du" : p.master ? "Spiel-Leiter" : !player.master ? "Mitspieler" :
+									content={ p.id === player.id ? "Du" : p.master ? "Spiel-Leiter" : !player.master ? `Mitspieler${ p.connected ? "" : " (Verbindung getrennt)" }` :
 										<span className="flex gap-2 items-center">
 											<span>Aktionen</span>
 											<Button title="Rauswerfen" color="danger" size="sm" onPress={ () => kick({ data: { id: p.id } }) }><UserX/></Button>
