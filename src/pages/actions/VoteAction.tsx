3191a9d6c287096d9e2d96779d649def81cde31a
MineKing <mineking9534@gmail.com>
2024-04-26 13:02:17 +0200

    Fix vote

--- src/pages/actions/VoteAction.tsx
+++ src/pages/actions/VoteAction.tsx
@@ -1,33 +1,24 @@
 import { Request } from "../../hooks/useRest.ts"
 import { Action, TargetContext } from "./Action.ts"
 import { useGameState } from "../../hooks/useGameState.ts"
-import { useContext, useEffect, useState } from "react"
+import { useContext } from "react"
 
 export default function useVoteAction(action: (req?: Request<unknown>) => void): Action {
 	const { game, player } = useGameState()!
 
-	const [ selected, setSelected ] = useState("")
 	const [ , setTargets ] = useContext(TargetContext)!
-
-	useEffect(() => {
-		setSelected("")
-	}, [ game.current ])
-
-	useEffect(() => {
-		setTargets([ selected ])
-	}, [ selected ])
 
 	return {
 		execute: target => {
 			if(!player.alive || !target.alive) return
 
 			return () => {
-				if(target.id !== selected) {
-					setSelected(target.id)
+				if(target.id !== game.interactions![player.id as keyof typeof game.interactions] as string) {
+					setTargets([ target.id ])
 					action({ data: { target: target.id } })
 				} else {
-					setSelected("")
-					action()
+					setTargets([])
+					action({ data: { target: "" } })
 				}
 			}
 		},
