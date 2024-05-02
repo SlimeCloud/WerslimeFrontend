a43f5ca3df6d96bad22dfe5ddad0235e4798c6fd
MineKing <mineking9534@gmail.com>
2024-05-02 14:07:42 +0200

    Improve websocket disconnect handling

--- src/components/EventProvider.tsx
+++ src/components/EventProvider.tsx
@@ -1,25 +1,35 @@
 import { EventSourceContext } from "../hooks/useEvent.ts";
 import { useToken } from "../hooks/useToken.ts";
-import { ReactNode, useEffect, useMemo, useState } from "react";
+import { ReactNode, useEffect, useMemo, useRef, useState } from "react";
 import { Button, Divider, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, useDisclosure } from "@nextui-org/react"
+import { useNavigate } from "react-router"
 
 export default function EventProvider({ route, children }: { route: string, children?: ReactNode }) {
+	const navigate = useNavigate()
+
 	const { isOpen, onOpen, onClose, onOpenChange } = useDisclosure()
 	const { token } = useToken()
 
+	const [ v, update ] = useState(0)
+	const ref = useRef<WebSocket>()
 	const [ event, setEvent ] = useState<CloseEvent>()
 
 	const source = useMemo(() => {
 		const ws = new WebSocket(`${ import.meta.env._WS }${ route }?token=${ token }`)
 		ws.onclose = event => {
+			console.log(event)
+
 			if(event.reason === "leave") return
 
 			setEvent(event)
 			onOpen()
 		}
 
+		if(ref.current?.OPEN) ref.current?.close(1000, "leave")
 		return ws
-	}, [ route, token ])
+	}, [ route, token, v ])
+
+	ref.current = source
 
 	useEffect(() => {
 		return () => source.close()
@@ -31,14 +41,14 @@ export default function EventProvider({ route, children }: { route: string, chil
 				{ children }
 			</EventSourceContext.Provider>
 
-			<Modal isOpen={ isOpen } onOpenChange={ onOpenChange }>
+			<Modal isOpen={ isOpen } onOpenChange={ onOpenChange } onClose={ () => navigate("/") }>
 				<ModalContent>
 					<ModalHeader className="py-3">Verbindung getrennt</ModalHeader>
 					<Divider/>
 
 					<ModalBody>
 						{ event?.reason
-							? <>Verbindung konnte nicht hergestellt werden: <b>{ event.reason }</b></>
+							? <>Verbindung konnte nicht hergestellt werden: <b className="text-danger">{ event.reason }</b></>
 							: <>Die Verbindung zum Server wurde Unterbrochen. Überprüfe deine Internet-Verbindung und stelle sicher, dass diese Seite nur in einem Tab geöffnet ist.</>
 						}
 					</ModalBody>
@@ -46,7 +56,7 @@ export default function EventProvider({ route, children }: { route: string, chil
 					<Divider/>
 					<ModalFooter className="px-4 py-2">
 						<Button color="primary" size="sm" onPress={ () => {
-							source.close()
+							update(x => x + 1)
 							onClose()
 						} }>Erneut Verbinden</Button>
 					</ModalFooter>
