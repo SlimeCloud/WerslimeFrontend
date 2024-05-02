758abe66ecc790ad75c51f88aa8228f77819d739
MineKing <mineking9534@gmail.com>
2024-05-02 16:45:57 +0200

    Improve settings

--- src/pages/GameLobby.tsx
+++ src/pages/GameLobby.tsx
@@ -2,12 +2,13 @@ import { useGameState } from "../hooks/useGameState.ts";
 import { Avatar, Button, Card, CardBody, CardHeader, Checkbox, CheckboxGroup, Divider, Popover, PopoverContent, PopoverTrigger, ScrollShadow, Slider, Tooltip, useDisclosure } from "@nextui-org/react";
 import { Crown, ShieldPlus, Unplug, UserX } from "lucide-react";
 import { Request, useRest } from "../hooks/useRest.ts";
-import { useEffect, useState } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import Spinner from "../components/Spinner.tsx";
 import { Role, roleDescriptions, roleNames, roleTeams, teamColors } from "../types/Role.ts"
 import { Player } from "../types/Player.ts"
 import ConditionalParent from "../components/ConditionalParent.tsx"
 import ErrorModal from "../components/ErrorModal.tsx"
+import { GameSettings } from "../types/Game.ts"
 
 export default function GameLobby() {
 	return (
@@ -68,8 +69,8 @@ function PlayerInfo({ p, kick, promote }: { p: Player, kick: (req: Request<unkno
 				placement="right" className="font-bold"
 				content={
 					p.id === player.id ? "Du" :
-					p.master ? "Spiel-Leiter" :
-					<>Mitspieler{ p.connected ? "" : <span className="text-default"> (Verbindung getrennt)</span> }</>
+						p.master ? "Spiel-Leiter" :
+							<>Mitspieler{ p.connected ? "" : <span className="text-default"> (Verbindung getrennt)</span> }</>
 				}
 			>
 				<span className={ `flex gap-2 items-center ${ p.id === player.id ? "font-bold" : "" }` }>
@@ -90,49 +91,11 @@ function Settings() {
 	const { isOpen, onOpen, onOpenChange } = useDisclosure()
 
 	const { state: startState, put: start } = useRest("/games/@me/session")
-	const { state: updateState, error, patch: update } = useRest("/games/@me", {
+	const { state: updateState, error, patch: update } = useRest<GameSettings>("/games/@me", {
 		onError: onOpen
 	})
 
 	const disabled = !player.master || updateState === "loading"
-
-	const [ amount, setAmount ] = useState<number>(game.settings.werewolfAmount)
-	const [ roles, setRoles ] = useState(game.settings.roles)
-
-	const [ isPublic, setPublic ] = useState(game.settings.isPublic)
-	const [ deadRoles, setDeadRoles ] = useState(game.settings.revealDeadRoles)
-	const [ deadSpectators, setDeadSpectators ] = useState(game.settings.deadSpectators)
-	const [ loverRoles, setLoverRoles ] = useState(game.settings.revealLoverRoles)
-	const [ muteMembers, setMuteMember ] = useState(game.settings.muteMembers)
-
-	function updateSettings(werewolfAmount?: number) {
-		update({
-			data: {
-				werewolfAmount: werewolfAmount || amount,
-				roles: roles,
-				isPublic: isPublic,
-				revealDeadRoles: deadRoles,
-				deadSpectators: deadSpectators,
-				revealLoverRoles: loverRoles,
-				muteMembers: muteMembers
-			}
-		})
-	}
-
-	useEffect(() => {
-		if(roles.toString() == game.settings.roles.toString() && isPublic === game.settings.isPublic && deadRoles === game.settings.revealDeadRoles && deadSpectators === game.settings.deadSpectators && loverRoles === game.settings.revealLoverRoles && muteMembers === game.settings.muteMembers) return
-		updateSettings()
-	}, [ roles, isPublic, deadRoles, deadSpectators, loverRoles, muteMembers ])
-
-	useEffect(() => {
-		setAmount(game.settings.werewolfAmount)
-		setRoles(game.settings.roles)
-		setPublic(game.settings.isPublic)
-		setDeadRoles(game.settings.revealDeadRoles)
-		setDeadSpectators(game.settings.deadSpectators)
-		setLoverRoles(game.settings.revealLoverRoles)
-		setMuteMember(game.settings.muteMembers)
-	}, [ game.settings ])
 
 	return (
 		<>
@@ -141,76 +104,118 @@ function Settings() {
 				<Divider/>
 				<CardBody className="flex flex-col justify-between gap-4">
 					<div className="flex flex-col gap-5 [&_h3]:font-bold">
-						<div>
-							<h3 className="flex flex-row justify-between">Werslime Anzahl <span>{ amount }</span></h3>
-							<Slider
-								aria-label="Werwolf Anzahl" className="font-bold [&_*]:!text-md" size="md"
-								minValue={ 1 } maxValue={ 10 } step={ 1 } showSteps
-								value={ amount }
-								onChange={ value => setAmount(value as number) }
-								onChangeEnd={ value => updateSettings(value as number) }
-								isDisabled={ disabled }
-							/>
-						</div>
+						<SettingsProperty<number> property="werewolfAmount" update={ update } autoUpdate={ false }>{ (value, setValue, update) =>
+							<div>
+								<h3 className="flex flex-row justify-between">Werslime Anzahl <span>{ value }</span></h3>
+								<Slider
+									aria-label="Werwolf Anzahl" className="font-bold [&_*]:!text-md" size="md"
+									minValue={ 1 } maxValue={ 10 } step={ 1 } showSteps
+									value={ value }
+									onChange={ value => setValue(value as number) }
+									onChangeEnd={ value => update(value as number) }
+									isDisabled={ disabled }
+								/>
+							</div> }
+						</SettingsProperty>
 
 						<div className="flex flex-col gap-4 justify-between xl:flex-row xl:[&>div]:w-1/2 [&>div]:flex [&>div]:flex-col [&>div]:gap-1">
-							<div>
-								<h3>Rollen</h3>
-								<CheckboxGroup
-									aria-label="Spezial-Rollen" size="md" className="[&>div]:gap-1"
-									value={ roles }
-									onValueChange={ values => setRoles(values as Role[]) }
-									isDisabled={ disabled }
-								>
-									{ [ ...roleDescriptions.entries() ].map(([ role, description ]) =>
-										<Tooltip key={ role } shouldFlip={ false } placement="right" content={ description }>
-											<div className="w-fit">
-												<Checkbox color={ teamColors.get(roleTeams.get(role)!) } value={ role }>{ roleNames.get(role) }</Checkbox>
-											</div>
-										</Tooltip>
-									) }
-								</CheckboxGroup>
-							</div>
+							<SettingsProperty<Role[]> property="roles" compare={ (a, b) => a.toString() == b.toString() } update={ update }>{ (value, setValue) =>
+								<div>
+									<h3>Rollen</h3>
+									<CheckboxGroup
+										aria-label="Spezial-Rollen" size="md" className="[&>div]:gap-1"
+										value={ value }
+										onValueChange={ values => setValue(values as Role[]) }
+										isDisabled={ disabled }
+									>
+										{ [ ...roleDescriptions.entries() ].map(([ role, description ]) =>
+											<Tooltip key={ role } shouldFlip={ false } placement="right" content={ description }>
+												<div className="w-fit">
+													<Checkbox color={ teamColors.get(roleTeams.get(role)!) } value={ role }>{ roleNames.get(role) }</Checkbox>
+												</div>
+											</Tooltip>
+										) }
+									</CheckboxGroup>
+								</div> }
+							</SettingsProperty>
 
 							<div>
 								<h3>Sonstiges</h3>
-								<Tooltip shouldFlip={ false } placement="right" content={ <span className="max-w-[400px]">Die Runde wird in 'Öffentliche Runden' angezeigt und kann ohne Link betreten werden</span> }>
-									<div className="w-fit">
-										<Checkbox isDisabled={ disabled } isSelected={ isPublic } onValueChange={ setPublic }>Öffentlich</Checkbox>
-									</div>
-								</Tooltip>
-								<Tooltip shouldFlip={ false } placement="right" content={ <>Rollen von Toten werden für alle angezeigt</> }>
-									<div className="w-fit">
-										<Checkbox isDisabled={ disabled } isSelected={ deadRoles } onValueChange={ setDeadRoles }>Tote Rollen anzeigen</Checkbox>
-									</div>
-								</Tooltip>
-								<Tooltip shouldFlip={ false } placement="right" content={ <>Tote können die Rollen Aller sehen</> }>
-									<div className="w-fit">
-										<Checkbox isDisabled={ disabled } isSelected={ deadSpectators } onValueChange={ setDeadSpectators }>Tote Zuschauer</Checkbox>
-									</div>
-								</Tooltip>
+								<BooleanProperty disabled={ disabled } property="isPublic" update={ update }
+									name={ <>Öffentlich</> }
+									description={ <span className="max-w-[400px]">Die Runde wird in 'Öffentliche Runden' angezeigt und kann ohne Link betreten werden</span> }
+								/>
+								<BooleanProperty disabled={ disabled } property="revealDeadRoles" update={ update }
+									name={ <>Tote Rollen anzeigen</> }
+									description={ <>Rollen von Toten werden für alle angezeigt</> }
+								/>
 
-								<Tooltip shouldFlip={ false } placement="right" content={ <>Die Verliebten sehen gegenseitig ihre Rollen</> }>
-									<div className="w-fit">
-										<Checkbox isDisabled={ disabled } isSelected={ loverRoles } onValueChange={ setLoverRoles }>Zeige Verliebten Rolle</Checkbox>
-									</div>
-								</Tooltip>
-
+								<BooleanProperty disabled={ disabled } property="deadSpectators" update={ update }
+									name={ <>Tote Zuschauer</> }
+									description={ <>Tote können die Rollen Aller sehen</> }
+								/>
+								<BooleanProperty disabled={ disabled } property="revealLoverRoles" update={ update }
+									name={ <>Zeige Verliebten Rolle</> }
+									description={ <>Die Verliebten sehen gegenseitig ihre Rollen</> }
+								/>
 								{ game.discord &&
-									<Tooltip shouldFlip={ false } placement="right" content={ <>Mitglieder werden automatisch gemutet, wenn sie nicht sprechen dürfen</> }>
-										<div className="w-fit">
-											<Checkbox isDisabled={ disabled } isSelected={ muteMembers } onValueChange={ setMuteMember }>Mitglieder muten</Checkbox>
-										</div>
-									</Tooltip>
+									<BooleanProperty disabled={ disabled } property="muteMembers" update={ update }
+										name={ <>Mitglieder muten</> }
+										description={ <>Mitglieder werden automatisch gemutet, wenn sie nicht sprechen dürfen</> }
+									/>
 								}
 							</div>
 						</div>
 					</div>
 
-					{ player.master && <Button className="font-bold min-h-[28px] flex-shrink-0" isDisabled={ amount >= game.players.length / 2.0 } isLoading={ startState === "loading" } spinner={ <Spinner/> } onPress={ () => start() }>Runde Starten</Button> }
+					{ player.master && <Button className="font-bold min-h-[28px] flex-shrink-0" isDisabled={ game.settings.werewolfAmount >= game.players.length / 2.0 } isLoading={ startState === "loading" } spinner={ <Spinner/> } onPress={ () => start() }>Runde Starten</Button> }
 				</CardBody>
 			</Card>
 			<ErrorModal error={ error! } isOpen={ isOpen } onOpenChange={ onOpenChange }/>
 		</>
+	)
+}
+
+function SettingsProperty<T>({ property, compare = (a, b) => a === b, update, autoUpdate = true, children }: {
+	property: keyof GameSettings,
+	compare?: (a: T, b: T) => boolean,
+	update: (req?: Request<GameSettings>) => void,
+	autoUpdate?: boolean,
+	children: (value: T, setValue: (value: T) => void, update: (value: T) => void) => ReactNode
+}) {
+	const { game } = useGameState()!
+	const actualValue = game.settings[property] as T
+
+	const [ value, setValue ] = useState<T>(game.settings[property] as T)
+
+	function doUpdate(value: T) {
+		update({
+			data: {
+				[property]: value
+			}
+		})
+	}
+
+	useEffect(() => {
+		setValue(actualValue)
+	}, [ actualValue ])
+
+	useEffect(() => {
+		if(compare(value, actualValue)) return
+		if(autoUpdate) doUpdate(value)
+	}, [ value ])
+
+	return children(value, setValue, doUpdate)
+}
+
+function BooleanProperty({ disabled, property, update, name, description }: { disabled: boolean, property: keyof GameSettings, update: (req?: Request<GameSettings>) => void, name: ReactNode, description: ReactNode }) {
+	return (
+		<SettingsProperty<boolean> property={ property } update={ update }>{ (value, setValue) =>
+			<Tooltip shouldFlip={ false } placement="right" content={ description }>
+				<div className="w-fit">
+					<Checkbox isDisabled={ disabled } isSelected={ value } onValueChange={ setValue }>{ name }</Checkbox>
+				</div>
+			</Tooltip> }
+		</SettingsProperty>
 	)
 }
