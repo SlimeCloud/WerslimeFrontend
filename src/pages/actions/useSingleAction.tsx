22ecf2acbaf5cdb0b32674026aca894882871667
MineKing <mineking9534@gmail.com>
2024-05-29 21:29:07 +0200

    Improve singeAction

--- src/pages/actions/useSingleAction.tsx
+++ src/pages/actions/useSingleAction.tsx
@@ -1,40 +1,28 @@
 import { Request } from "../../hooks/useRest.ts"
-import { Button, Card, CardBody, CardHeader, Divider, Image, Modal, ModalBody, ModalContent, ModalHeader, useDisclosure } from "@nextui-org/react"
-import { useContext, useEffect, useState } from "react"
-import { PlayerClickHandler, TargetContext } from "./PlayerClickHandler.ts"
+import { Card, CardBody, CardHeader, Divider, Image, Modal, ModalBody, ModalContent, ModalHeader, useDisclosure } from "@nextui-org/react"
+import { useState } from "react"
+import { PlayerClickHandler } from "./PlayerClickHandler.ts"
 import { useGameState } from "../../hooks/useGameState.ts"
 import { EMPTY_PLAYER, Player } from "../../types/Player.ts"
 import { Action } from "./useMultiAction.tsx"
 
 export default function useSingleAction(action: (req?: Request<unknown>) => void, condition: (target: Player) => boolean | undefined, actions: Action): PlayerClickHandler {
 	const { game } = useGameState()!
-	const [ , setTargets ] = useContext(TargetContext)!
 	const { isOpen, onOpen, onClose, onOpenChange } = useDisclosure()
 
 	const [ target, setTarget ] = useState<Player>(EMPTY_PLAYER)
-	const [ selected, setSelected ] = useState("")
 
-	useEffect(() => {
-		setSelected("")
-	}, [ game.current ])
-
-	useEffect(() => {
-		setTargets([ selected ])
-		onClose()
-	}, [ selected ]);
-
-	function confirm() {
+	function confirm(selected: string) {
 		action({
-			data: { [actions.id]: selected },
-			onSuccess: () => setSelected("")
+			data: { [actions.id]: selected }
 		})
+		onClose()
 	}
 
 	return {
 		execute: target => {
 			if(game.interacted) return
 			if(!condition(target)) return
-			if(target.id === selected) return
 
 			return () => {
 				setTarget(target)
@@ -47,7 +35,7 @@ export default function useSingleAction(action: (req?: Request<unknown>) => void
 					<ModalHeader className="py-3 font-bold flex justify-center text-xl">Wähle eine Aktion</ModalHeader>
 					<ModalBody className="grid grid-cols-3 gap-2 px-10 py-5">
 						<span/>
-						<Card className="hover:scale-[1.05] mx-auto bg-default-100" isPressable onPress={ () => setSelected(target.id) } isDisabled={ actions.condition && !actions.condition(target) }>
+						<Card className="hover:scale-[1.05] mx-auto bg-default-100" isPressable onPress={ () => confirm(target.id) } isDisabled={ actions.condition && !actions.condition(target) }>
 							<CardHeader className="font-bold flex justify-center px-5 whitespace-nowrap">{ actions.name }</CardHeader>
 							<Divider/>
 							<CardBody>
@@ -57,7 +45,6 @@ export default function useSingleAction(action: (req?: Request<unknown>) => void
 					</ModalBody>
 				</ModalContent>
 			</Modal>
-			{ !game.interacted && <Button color={ selected ? "primary" : "warning" } className="fixed bottom-[60px] left-5 z-20" onPress={ confirm }>Bestätigen</Button> }
 		</>
 	}
 }
